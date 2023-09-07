db79f2bbfa45e978becec8114e02f2d0d3963cb0
Villain66 <124810879+Villain66@users.noreply.github.com>
2023-09-07 11:22:28 +0200

    exo7 fini

---
+++ exo7/exo7.js
@@ -0,0 +1,9 @@
+var personne = {
+    nom: "Bruce Wayne",
+    age: 30,
+    ville: "Gotham"
+}
+
+console.log("Nom : " + personne.nom);
+console.log("Age : " + personne.age + " ans");
+console.log("Ville : " + personne.ville);
