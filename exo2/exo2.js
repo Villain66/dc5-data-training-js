557176dd725e38e31d093132af6b1b1ba939b841
Villain66 <124810879+Villain66@users.noreply.github.com>
2023-09-07 10:14:59 +0200

    exo 2 fini

---
+++ exo2/exo2.js
@@ -0,0 +1,10 @@
+var nombre1 = 6
+var nombre2 = 3
+
+var somme = nombre1 + nombre2
+var soustraction = nombre1 - nombre2
+var multiplication = nombre1 * nombre2
+
+console.log("Somme : " + somme);
+console.log("Soustraction : " + soustraction);
+console.log("Multiplication : " + multiplication);
