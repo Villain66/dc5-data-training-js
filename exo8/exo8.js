19fe4c6201d3927624c7bd6466ea27a7eb0ba28a
Villain66 <124810879+Villain66@users.noreply.github.com>
2023-09-07 11:35:46 +0200

    exo8 fini

---
+++ exo8/exo8.js
@@ -0,0 +1,9 @@
+const calculerSomme = (nombre1, nombre2) => nombre1 + nombre2;
+
+// Utiliser la fonction avec deux nombres
+var resultat = calculerSomme(5, 3);
+
+// Afficher le résultat dans la console
+console.log("La somme est : " + resultat);
+
+//code généré via chatgpt pour valider l'exo afin de ne pas perdre de trop de temps sur la fonction fléchée qui n'avait jamais été vue
